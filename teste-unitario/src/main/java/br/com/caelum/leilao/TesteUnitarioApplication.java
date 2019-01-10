5316c36a43b72a64c9c7531d25d7cf18457c0db9
Carlos Antunes <CEAntunes.WA@rd.com.br>
2019-01-10 19:06:19 -0200

    teste unitario junit

---
+++ teste-unitario/src/main/java/br/com/caelum/leilao/TesteUnitarioApplication.java
@@ -0,0 +1,14 @@
+package br.com.caelum.leilao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUnitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteUnitarioApplication.class, args);
+	}
+
+}
+
